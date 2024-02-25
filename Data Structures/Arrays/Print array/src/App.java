9c3ec5c34ad4554e969c57140526a6ea1cf52950
iamdul <kalharidulanga@gmail.com>
2024-02-25 11:02:40 +0530

    print array

--- Data Structures/Arrays/Print array/src/App.java
+++ Data Structures/Arrays/Print array/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] arr={78,45,89};
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr){
+        System.out.print("[");
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\b]");
     }
 }
